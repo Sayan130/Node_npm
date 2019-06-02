6b39cf1cb6462b85181a370822a2d857f4c3f383
Sayan130 <rick.alipurduar@gmail.com>
2019-06-03 00:02:50 +0530

    Mail system

--- Http.js
+++ Http.js
@@ -1,1 +1,25 @@
-console.log("Hello Node")
+let nodemailer = require("nodemailer");
+
+let transport =  nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'rick.alipurduar@gmail.com',
+      pass: 'Sayan@1406130'
+    }
+  })
+
+let mail = {
+    from :"rick.alipurduar@gmail.com",
+    to: "rick.alipurduar@gmail.com, siddharthdutta55@gmail.com",
+    subject : "Hello from Sayan Sarkar",
+    text : "Hey you!!Have a nice day"
+}
+
+transport.sendMail(mail, (err, inf)=>{
+
+    if(err)console.log("Unable to send "+err)
+    else{
+        console.log("Mail has been sent to the user");
+    }
+
+})
